117d57816d63cab6a5d9d45b5a4bb8c82d089cd5
mirror12k <mirror12k@gmail.com>
2026-01-18 11:17:51 -0500

    - tweak

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -39,14 +39,14 @@ resource "aws_securityhub_standards_subscription" "account_securityhub_aws_found
   standards_arn = "arn:aws:securityhub:us-east-1::standards/aws-foundational-security-best-practices/v/1.0.0"
   depends_on    = [aws_securityhub_account.account_securityhub]
 }
-resource "aws_securityhub_standards_subscription" "account_securityhub_cis_subscription" {
-  standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
-  depends_on    = [aws_securityhub_account.account_securityhub]
-}
-resource "aws_securityhub_standards_subscription" "account_securityhub_pci_subscription" {
-  standards_arn = "arn:aws:securityhub:us-east-1::standards/pci-dss/v/3.2.1"
-  depends_on    = [aws_securityhub_account.account_securityhub]
-}
+# resource "aws_securityhub_standards_subscription" "account_securityhub_cis_subscription" {
+#   standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
+#   depends_on    = [aws_securityhub_account.account_securityhub]
+# }
+# resource "aws_securityhub_standards_subscription" "account_securityhub_pci_subscription" {
+#   standards_arn = "arn:aws:securityhub:us-east-1::standards/pci-dss/v/3.2.1"
+#   depends_on    = [aws_securityhub_account.account_securityhub]
+# }
 
 # a random tfstate bucket to hold any terraform states that we create with infrastructure
 resource "random_id" "account_tfstate_bucket_random_id" { byte_length = 24 }
